d786a6e62a1675d93631e53fc0d1afd7f9396ad1
Chhitij <saraswatchhitij@gmail.com>
2018-10-20 08:50:45 +0530

    face identification trial 1

---
+++ kerasClassifier.py
@@ -0,0 +1,71 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
+import scipy.ndimage
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=(300, 300, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# the model so far outputs 3D feature maps (height, width, features)
+
+model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+# COMPILE
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+
+batch_size = 16
+
+# this is the augmentation configuration we will use for training
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+# this is a generator that will read pictures found in subfolers of 'data/train', and indefinitely generate
+# batches of augmented image data
+train_generator = train_datagen.flow_from_directory(
+        'training_images',  # this is the target directory
+        target_size=(300, 300),  # all images will be resized to 300x300
+        batch_size=batch_size,
+        class_mode='binary')  # since we use binary_crossentropy loss, we need binary labels
+
+# this is a similar generator, for validation data
+validation_generator = test_datagen.flow_from_directory(
+        'validation',
+        target_size=(300, 300),
+        batch_size=batch_size,
+        class_mode='binary')
+
+
+model.fit_generator(
+        train_generator,
+        steps_per_epoch = 2000 // batch_size,
+        epochs=100,
+        validation_data=validation_generator,
+        validation_steps=80 // batch_size)
+
+model.save('chhitij_detector.h5')  # always save your model after training or during training
+
